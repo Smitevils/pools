952e7a9ff65f30fe709cca0ec76743b7f27b246a
smitevils <smitevils@yandex.ru>
2015-07-19 15:28:52 +0300

    repeat coding

--- assets/layers/main.php
+++ assets/layers/main.php
@@ -24,28 +24,92 @@
 		</div>
 	</div>
 </div>
+<div class="slider">
+	sfd
+</div>
+<div class="production">
+	<div class="block-title">
+		<h4>Продукция</h4>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="product-block">
+					<div class="product-img">
+						<img src="assets/img/products/product-1.jpg" alt="product">
+					</div>
+					<div class="product-text">
+						<h6>Модель “Африка”</h6>
+						<p>Aenean sollicitudin, lorem quis bibendum auctornisi elit conse quat ipsum, nec sagittis sem nibh id elit.</p>
+					</div>
+					<div class="product-price">
+						15 000 P
+					</div>
+					<div class="choise-btn"></div>
+				</div>
+			</div>
+			<div class="col-md-3"></div>
+			<div class="col-md-3"></div>
+			<div class="col-md-3"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-3 col-md-3 text-center">
+				<div class="choise-complectation">
+					<h6>Комплектующие</h6>
+					<label for="check1">Насос</label><input type="checkbox" id="check1">
+					<label for="check2">Фильтр</label><input type="checkbox" id="check2">
+					<label for="check3">Блок очистки</label><input type="checkbox" id="check3">
+				</div>
+			</div>
+			<div class="col-md-3 text-center">
+				<div class="cost">
+					<h5>Стоимость</h5>
+					<div class="cost-block">
+						15 000 P
+					</div>
+					<div class="bid-btn"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="gallery bg-blue">
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
-	<br>
 	<div class="block-title">
 		<h4>Фотогалерея</h4>
 	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
-				<a class="fancybox" href="1_b.jpg" data-fancybox-group="gallery" title="1-я кртинка"><img src="1_s.jpg" alt="" /></a>
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
 			</div>
-			<div class="col-md-3">2</div>
-			<div class="col-md-3">3</div>
-			<div class="col-md-3">4</div>
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+			<div class="col-md-3">
+				<a class="fancybox" href="assets/img/gallery/big/photo-1.jpg" data-fancybox-group="gallery" title=""><img src="assets/img/gallery/small/photo-1.jpg" alt="gallery" /></a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="all-photo-btn"></div>
+			</div>
 		</div>
 	</div>
 </div>
@@ -105,6 +169,47 @@
 					<img src="assets/img/icons/advantages-6.png" alt="img">
 					<h3>свое производство</h3>
 				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="download">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<a href="#" target="_blank">
+					<div class="download-img"></div>
+				</a>
+				<div class="download-block">
+					<h6>Cкачайте презентацию</h6>
+					<p>и убедитесь как просто устанавливается наш бассейн</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="map">
+	<div class="map-container">
+		<script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=KIUaIToWaF0kGZQe0DV1RU0aYLdAUgK4&width=100%&height=500"></script>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="contacts-block">
+					<p class="phone">+7 (812) 777 999 66</p>
+					<p class="adress">СПб, ул. 2-ая Советская 27/2</p>
+					<p class="email">info@greenpools.ru</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p>© ООО "GREENPOOLS" 2015</p>
+				<p>Разработка сайта <a href="http://skipodevelop.ru/" target="_blank">SkipoDevelop.ru</a></p>
 			</div>
 		</div>
 	</div>
